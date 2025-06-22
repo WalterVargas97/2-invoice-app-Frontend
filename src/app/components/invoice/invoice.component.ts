2865233e9636c74dbb13b874b294bd09f23c9b77
Walter Vargas <walterbolivar97@gmail.com>
2025-06-22 15:21:28 -0500

    [feat]: add diferent components to invoice component as imports

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { InvoiceService } from '../../services/invoice.service';
 import { OnInit } from '@angular/core';
 import { Invoice } from '../../models/invoice';
+import { InvoiceViewComponent } from '../invoice-view/invoice-view.component';
+import { ClientViewComponent } from '../client-view/client-view.component';
+import { ListItemsViewComponent } from '../list-items-view/list-items-view.component';
+import { RowItemComponent } from '../row-item/row-item.component';
+import { CompanyViewComponent } from '../company-view/company-view.component';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [],
+  imports: [InvoiceViewComponent, ClientViewComponent, ListItemsViewComponent, RowItemComponent, CompanyViewComponent],
   templateUrl: './invoice.component.html',
   styleUrl: './invoice.component.css'
 })
