c1b9406e01102c97f3c3aaa3228c4fe5cce01a07
Walter Vargas <walterbolivar97@gmail.com>
2025-06-22 15:18:29 -0500

    [feat]: create diferents components view

---
+++ src/app/components/list-items-view/list-items-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-items-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-items-view.component.html',
+  styleUrl: './list-items-view.component.css'
+})
+export class ListItemsViewComponent {
+
+}
